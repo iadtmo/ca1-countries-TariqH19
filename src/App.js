7c6e577bbc8949d98c42a7eb74fb26249c2e14b5
Tariq Horan <N00202437@iadt.ie>
2022-10-21 13:08:02 +0100

    Setup react countries app for ca, installed axios to consume thecountries api, installed react bootstrap to style app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./assets/app.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Row";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="mt-5">
+      <Row>
+        <Col></Col>
+      </Row>
+    </Container>
   );
-}
+};
 
 export default App;
